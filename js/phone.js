ed5b6490f0998cef5445ed26224f5e19cbe14788
Asif Imtiaz Shaafi <asifimtiaz_shaafi1@baylor.edu>
2023-07-14 22:21:52 -0500

    Complete Step 3 of assignment

--- js/phone.js
+++ js/phone.js
@@ -2,26 +2,31 @@ $(document).ready(function () {
   $('#content_dialer').show();
   $('#content_list').hide();
   $('#content_add').hide();
+  $('#test_gesture').hide();
 });
 
 $('#dial_bar').click(function () {
   $('#content_dialer').show();
   $('#content_list').hide();
   $('#content_add').hide();
+  $('#test_gesture').hide();
 
   // change color of clicked button and reset others
   $(this).css('background-color', '#e6e6e6');
   $('#contact_bar').css('background-color', 'white');
   $('#add_contact_bar').css('background-color', 'white');
+  $('#gesture_bar').css('background-color', 'white');
 });
 
 $('#contact_bar').click(function () {
   $('#content_dialer').hide();
   $('#content_list').show();
   $('#content_add').hide();
+  $('#test_gesture').hide();
   // change color of clicked button and reset others
   $(this).css('background-color', '#e6e6e6');
   $('#dial_bar').css('background-color', 'white');
+  $('#gesture_bar').css('background-color', 'white');
   $('#add_contact_bar').css('background-color', 'white');
 });
 
@@ -29,10 +34,24 @@ $('#add_contact_bar').click(function () {
   $('#content_dialer').hide();
   $('#content_list').hide();
   $('#content_add').show();
+  $('#test_gesture').hide();
+  // change color of clicked button and reset others
+  $(this).css('background-color', '#e6e6e6');
+  $('#dial_bar').css('background-color', 'white');
+  $('#gesture_bar').css('background-color', 'white');
+  $('#contact_bar').css('background-color', 'white');
+});
+
+$('#gesture_bar').click(function () {
+  $('#test_gesture').show();
+  $('#content_dialer').hide();
+  $('#content_list').hide();
+  $('#content_add').hide();
   // change color of clicked button and reset others
   $(this).css('background-color', '#e6e6e6');
   $('#dial_bar').css('background-color', 'white');
   $('#contact_bar').css('background-color', 'white');
+  $('#add_contact_bar').css('background-color', 'white');
 });
 
 var count = 0;
@@ -72,4 +91,13 @@ $('#dial-button').on('click', function () {
   } else {
     alert('Calling ' + lastVal + '...');
   }
+});
+
+// Gesture Recognition in the gesture_area div
+$('#gesture_area').mousedown(function () {
+  $('#gesture_output').val('Mouse Down');
+});
+
+$('#gesture_area').mouseup(function () {
+  $('#gesture_output').val('Mouse Up');
 });
