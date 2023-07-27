0041db61effd5b6b3b7bd444597c9c404e77c7bf
Asif Imtiaz Shaafi <asifimtiaz_shaafi1@baylor.edu>
2023-07-26 20:39:27 -0500

    Remove all code of assignment 4

--- js/phone.js
+++ js/phone.js
@@ -70,13 +70,6 @@ $('.clear-button').on('click', function () {
   $('#contact_output_text').val('');
 });
 
-// $('#contact-form-submit-button').on('click', function () {
-//   alert('Contact added!');
-//   $('#name').val('');
-//   $('#email').val('');
-//   $('#phone').val('');
-// });
-
 $('#contact-form-clear-button').on('click', function () {
   $('#name').val('');
   $('#email').val('');
@@ -90,92 +83,4 @@ $('#dial-button').on('click', function () {
   } else {
     alert('Calling ' + lastVal + '...');
   }
-});
-
-// Gesture Recognition in the gesture_area div
-
-// Handling the mouse swipes
-downX = 0;
-downY = 0;
-
-navDownX = 0;
-
-$('#gesture_area').mousedown(function (e) {
-  downX = e.pageX;
-  downY = e.pageY;
-  $('#gesture_output').val('Mouse Down');
-});
-
-$('#gesture_area_up_down').mousedown(function (e) {
-  downX = e.pageX;
-  downY = e.pageY;
-  $('#gesture_output').val('Mouse Down');
-});
-
-$('#gesture_area').mouseup(function (e) {
-  upX = e.pageX;
-  upY = e.pageY;
-
-  output = '';
-
-  if (upX < downX) {
-    output = 'Swipe Left';
-  } else if (upX > downX) {
-    output = 'Swipe Right';
-  } else if (upX == downX) {
-    output = 'Mouse Up';
-  }
-
-  $('#gesture_output').val(output);
-});
-
-$('#gesture_area_up_down').mouseup(function (e) {
-  upX = e.pageX;
-  upY = e.pageY;
-
-  output = '';
-
-  if (upY < downY) {
-    output = 'Swipe Up';
-  } else if (upY > downY) {
-    output = 'Swipe Down';
-  } else if (upY == downY) {
-    output = 'Mouse Up';
-  }
-
-  $('#gesture_output').val(output);
-});
-
-$('#gesture_nav').mousedown(function (e) {
-  navDownX = e.pageX;
-});
-
-$('#gesture_nav').mouseup(function (e) {
-  navUpX = e.pageX;
-
-  output = 0;
-
-  tabs = ['dial_bar', 'contact_bar', 'add_contact_bar', 'gesture_bar'];
-
-  if (navUpX < navDownX) {
-    output = -1;
-    console.log('swipe left');
-  } else if (navUpX > navDownX) {
-    output = 1;
-    console.log('swipe right');
-  } else if (navUpX == navDownX) {
-    output = 0;
-  }
-
-  currentTabIndex += output;
-  console.log('currentTabIndex after swipe = ' + currentTabIndex);
-
-  if (currentTabIndex < 0) {
-    currentTabIndex = 3;
-  } else if (currentTabIndex > 3) {
-    currentTabIndex = 0;
-  }
-
-  selectTab(tabIds[currentTabIndex]);
-
 });
